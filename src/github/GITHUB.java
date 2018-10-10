1495453ec9d2c822a3c0e7a4ebe39dc77e583925
COSLAB <COSLAB@134.154.250.212>
2018-10-10 11:46:13 -0700

    hahs

--- src/github/GITHUB.java
+++ src/github/GITHUB.java
@@ -15,7 +15,7 @@ public class GITHUB {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Username:sphein; Name: Saengyao");
+        System.out.println("Username:sphein; Name: Saengyao Phein");
         
     }
     
